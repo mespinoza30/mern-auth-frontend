1b5f2b1491273630827b6609eac5509f7eb071db
mespinoza30 <m.espinoza16@hotmail.com>
2021-03-05 09:41:35 -0800

    Profile

--- src/App.js
+++ src/App.js
@@ -8,6 +8,13 @@ import setAuthToken from './utils/setAuthToken';
 import './App.css';
 
 // Components
+import Signup from './components/Signup';
+import About from './components/About';
+import Footer from './components/Footer';
+import Login from './components/Login';
+import Navbar from './components/Navbar';
+import Profile from './components/Profile';
+import Welcome from './components/Welcome';
 
 function App() {
   // Set state values
@@ -19,7 +26,7 @@ function App() {
   return (
     <div className="App">
       <h1>MERN Authentication</h1>
-      
+    <Login />
     </div>
   );
 }
